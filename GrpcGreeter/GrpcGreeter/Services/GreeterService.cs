dde344fb5fd03736d58b2096ce1d760a3f19635e
Iasonas <iasonas.ps@gmail.com>
2023-07-09 15:01:48 +0300

    Both commited

---
+++ GrpcGreeter/GrpcGreeter/Services/GreeterService.cs
@@ -0,0 +1,28 @@
+using Grpc.Core;
+using GrpcGreeter;
+
+namespace GrpcGreeter.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        private static int GrpcServiceIndex = 0;
+        // The GrpcServiceIndex is an attempt to manually keep track of the service state
+        //,since gRpc service is stateless
+
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            GrpcServiceIndex++;
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Grpc Service Index is : {GrpcServiceIndex.ToString()}",
+            });
+        }
+    }
+}
---
+++ GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+
+
+namespace GrpcGreeterClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7097");
+
+            var client = new Greeter.GreeterClient(channel);
+
+
+            bool control = true;
+
+            while (control)
+            {
+                var message = Console.ReadLine();
+
+                var reply = client.SayHello(
+                   new HelloRequest
+                   {
+                       Name = message,
+                   });
+                Console.WriteLine("- - - - - - - - - - - - - - - - - - ");
+                Console.WriteLine(reply.Message);
+                Console.WriteLine("- - - - - - - - - - - - - - - - - - ");
+            }
+
+        }
+    }
+}
